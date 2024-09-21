4369450cfeffb04258cb624dd7e7346171ce2d22
Kidus Mekonnen <kidusmekonnen591@gmail.com>
2024-09-21 13:19:24 +0300

    cancel notification on logout

--- api/storage.js
+++ api/storage.js
@@ -6,6 +6,7 @@ import { HOME_STRINGS } from '~/constants/strings/home/home';
 import { REMINDER_STRINGS } from '~/constants/strings/home/reminder';
 import { TECHNIQUES_STRINGS } from '~/constants/strings/home/self care/techniques';
 import { SETTING_STRINGS } from '~/constants/strings/setting';
+import { cancelNotification } from '~/utils/notification';
 
 export const setToken = async (token) => {
   if (token) {
@@ -52,6 +53,7 @@ export const setRole = async (role) => {
 };
 
 export const logout = async () => {
+  await cancelNotification();
   await AsyncStorage.clear();
   // await initilizeSettings();
 };
